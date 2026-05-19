f69992b91559d1a787a84a6d02b342765fbb79e1
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-05-19 14:32:57 +0200

    add test for if expressions

---
+++ tests/rust/if.rs
@@ -0,0 +1,15 @@
+fn main() -> usize {
+    if 4 < 6 {
+        if 4 == 5 {
+            1
+        } else if 0 != 0 {
+            2
+        } else if 7 == 7 {
+            if false { 3 } else { 42 }
+        } else {
+            4
+        }
+    } else {
+        5
+    }
+}
