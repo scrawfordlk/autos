54bd2824b3be1f9cf7cb6a7cdb87c27b295c5c45
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-05-20 22:12:56 +0200

    make mixed if more complicated
    This has better coverage for if expressions with mixed return types.

--- tests/rust/if_mixed.rs
+++ tests/rust/if_mixed.rs
@@ -1,7 +1,18 @@
 fn main() -> usize {
     if false {
         return 1; // type: !
-    } else {
-        42 // type: usize
-    }
+    } else
+        // type: usize
+        if true {
+            if false {
+                3
+            } else
+                if false {
+                    return 2;
+                } else {
+                    42
+                }
+        } else {
+            return 3;
+        }
 }
