f9c56a0fe27f6e9938f28a2311af2933741d3ecf
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-05-21 19:05:08 +0200

    have match test with final arm being a literal

--- tests/rust/match.rs
+++ tests/rust/match.rs
@@ -1,18 +1,18 @@
 fn main() -> usize {
     let n: u8 = 4 as u8;
-    let x: bool = true;
+    let x: bool = false;
     let c: char = 'a';
 
     match n as usize {
         10 => return 1,
         4 => match x {
-            true => match c {
+            true => 2,
+            false => match c {
                 'b' => 4,
                 'a' => return 42,
                 _ => 5,
             },
-            false => 2,
         },
-        100 => return 0,
+        100 => return 6,
     }
 }
