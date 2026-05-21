8f6d25cc9d611c37a105fe15b4de3df3b1967505
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-05-21 19:42:29 +0200

    annotate mixed return type if with type hints

--- tests/rust/if_mixed.rs
+++ tests/rust/if_mixed.rs
@@ -1,18 +1,20 @@
 fn main() -> usize {
+    // type: usize
     if false {
         return 1; // type: !
     } else
         // type: usize
         if true {
+            // type: usize
             if false {
-                3
+                return 3;    // type: !
             } else
                 if false {
-                    return 2;
+                    return 2; // type: !
                 } else {
-                    42
+                    42        // type: usize
                 }
         } else {
-            return 3;
+            return 3; // type: !
         }
 }
