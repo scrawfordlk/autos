09b0742d99b4d96101fdc8abd1f47a4ed026c6c2
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-05-21 19:43:45 +0200

    inline variables in test

--- tests/rust/match.rs
+++ tests/rust/match.rs
@@ -1,13 +1,11 @@
 fn main() -> usize {
     let n: u8 = 4 as u8;
-    let x: bool = false;
-    let c: char = 'a';
 
     match n as usize {
         10 => return 1,
-        4 => match x {
+        4 => match false {
             true => 2,
-            false => match c {
+            false => match 'a' {
                 'b' => 4,
                 'a' => return 42,
                 _ => 5,
