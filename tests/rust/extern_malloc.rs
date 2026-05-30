f891bf37ef943442774a2757d5caeecb35d18044
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-05-30 18:27:15 +0200

    make malloc test more complicated
    Originally simplified, because there was no support for any kind of
    arithmetic with pointers. Now pointers can be casted to/from usize, so
    use that to do arithmetic on pointers.

--- tests/rust/extern_malloc.rs
+++ tests/rust/extern_malloc.rs
@@ -7,24 +7,16 @@ fn wrapper(usize_count: usize) -> *mut usize {
 }
 
 fn main() -> usize {
-    let ptr: *mut usize = wrapper(4);
     unsafe {
-        *ptr = 42;
-        *ptr
+        let p: *mut usize = wrapper(4);
+        *p = 21;
+        *ptr_add(p, 1) = 21;
+        *ptr_add(p, 2) = 10;
+        *ptr_add(p, 3) = 11;
+        *p + *ptr_add(p, 1) + *ptr_add(p, 2) + *ptr_add(p, 3)
     }
 }
 
-// fn main() -> usize {
-//     unsafe {
-//         let ptr: *mut usize = wrapper(4);
-//         *ptr = 21;
-//         *ptr_add(ptr, 1) = 21;
-//         *ptr_add(ptr, 2) = 10;
-//         *ptr_add(ptr, 3) = 11;
-//         *ptr + *ptr_add(ptr, 1) + *ptr_add(ptr, 2) + *ptr_add(ptr, 3)
-//     }
-// }
-//
-// fn ptr_add(ptr: *mut usize, offset: usize) -> *mut usize {
-//     (ptr as usize + offset * 8) as *mut usize // TODO: do not hardcode usize size
-// }
+fn ptr_add(p: *mut usize, offset: usize) -> *mut usize {
+    (p as usize + offset * 8) as *mut usize // TODO: do not hardcode usize size
+}
