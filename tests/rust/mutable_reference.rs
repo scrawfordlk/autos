3eb7b474c31268513bad0516a60ec29ed7c62ff6
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-06-01 14:02:03 +0200

    fix mutable_reference test

--- tests/rust/mutable_reference.rs
+++ tests/rust/mutable_reference.rs
@@ -1,12 +1,9 @@
 fn test() -> usize {
-    let mut x: usize = 0;
-    let mut seen: usize = {
-        let ref_x: &usize = &x;
-        x = 41;
-        *ref_x
-    };
-    let mut_ref_x: &mut usize = &mut x;
-    *mut_ref_x = seen + 1;
+    let mut x: usize = 41;
+    {
+        let mut_ref_x: &mut usize = &mut x;
+        *mut_ref_x = *mut_ref_x + 1;
+    }
     x
 }
 
