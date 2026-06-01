4bdd8b5b3de1c0dd9bc181cb8362400389e2c8f9
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-06-01 13:58:45 +0200

    fix invalid cast
    Rust does not allow casting a shared reference to a mutable raw pointer.

--- tests/rust/cast_ptr_usize.rs
+++ tests/rust/cast_ptr_usize.rs
@@ -1,6 +1,6 @@
 fn test() -> usize {
-    let x: usize = 41;
-    let p: *mut usize = &x as *mut usize;
+    let mut x: usize = 41;
+    let p: *mut usize = &mut x as *mut usize;
     let addr: usize = p as usize;
     let p2: *mut usize = addr as *mut usize;
     unsafe {
