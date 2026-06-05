1482c90d89c9b39f9d17dec2882d05d04f0521a9
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-06-05 20:27:11 +0200

    improve coverage of match statement test
    Check:
    - shadowed binding
    - mutable binding

--- tests/rust/match_statement.rs
+++ tests/rust/match_statement.rs
@@ -1,14 +1,30 @@
 fn test() -> usize {
-    let mut x: usize = 21;
-    let y: usize = 1;
+    let mut x: usize = 19;
+    let y: usize = 0;
 
     match x {
         100 => return 2,
         4 => x = 3,
+        mut z => {
+            z = 1;
+            x = x + z
+        },
+    }
+
+    let z: usize = 0;
+    match x {
+        mut z => {
+            z = 1;
+            x = x + z;
+        },
+    }
+
+    match x {
+        20 => return 5,
         y => x = x + y,
     }
 
-    x
+    x + z
 }
 
 fn main() {
