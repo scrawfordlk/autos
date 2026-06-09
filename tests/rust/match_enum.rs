8f902d92c2a2d81953348a25c03b7cbdceb8610e
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-06-09 10:31:11 +0200

    increase complexity of enum match test

--- tests/rust/match_enum.rs
+++ tests/rust/match_enum.rs
@@ -1,18 +1,18 @@
 enum Colour {
-    Red,
-    Green,
+    Red(usize, usize),
+    Green(usize),
     Blue,
 }
 
 fn test() -> usize {
-    let g: Colour = Colour::Green;
+    let g: Colour = Colour::Green(42);
     match g {
-        Colour::Red | Colour::Blue => 1,
-        Colour::Green => match Colour::Blue {
-            Colour::Green | Colour::Blue | Colour::Red => match Colour::Red {
+        Colour::Red(_, _) | Colour::Blue => 1,
+        Colour::Green(n) => match Colour::Blue {
+            Colour::Green(_) | Colour::Blue | Colour::Red(_, _) => match Colour::Red(5, 6) {
                 Colour::Blue => 2,
-                Colour::Green | Colour::Red => match Colour::Green {
-                    Colour::Red | Colour::Green => 42,
+                Colour::Green(_) | Colour::Red(_, _) => match Colour::Green(4) {
+                    Colour::Red(_, _) | Colour::Green(_) => n,
                     Colour::Blue => 3,
                 },
             },
