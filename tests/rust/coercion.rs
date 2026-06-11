3782fcd51355c55b42d684d86273d94e7bcd3d24
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-06-12 00:56:38 +0200

    add a test for coercions

---
+++ tests/rust/coercion.rs
@@ -0,0 +1,32 @@
+fn test() -> usize {
+    let mut n: usize = 3;
+    let r: &usize = &mut n;
+    g();
+    if *r == 0 {
+        unsafe { exit(2) }
+    } else {
+        *r + f(&mut n)
+    }
+}
+
+fn f(x: &usize) -> usize {
+    *x
+}
+
+enum Option {
+    Some(usize),
+    None,
+}
+
+fn g() {
+    return;
+    let opt: Option = Option::Some(unsafe { exit(4) });
+}
+
+fn main() {
+    unsafe { exit(test()) }
+}
+
+unsafe extern "C" {
+    fn exit(code: usize) -> !;
+}
