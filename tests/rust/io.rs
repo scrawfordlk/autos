324742d14fe25cb26db51b657eeba82de126d98f
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-06-14 15:16:49 +0200

    improve robustness of I/O test

--- tests/rust/io.rs
+++ tests/rust/io.rs
@@ -3,6 +3,7 @@ fn test() -> usize {
     let O_RDONLY: usize = 0;
     let O_WRONLY_CREAT_TRUNC: usize = 321; // O_WRONLY = 1, O_CREAT = 64, O_TRUNC = 256
     let mode: usize = 420; // = 0o0644
+    let usize_MAX: usize = 18446744073709551615; // = -1
 
     unsafe {
         let fd: usize = open(
@@ -10,24 +11,26 @@ fn test() -> usize {
             O_WRONLY_CREAT_TRUNC,
             mode,
         );
+        if fd == usize_MAX {
+            return 1;
+        }
         let mut c: char = '*';
         let x: usize = write(fd, &mut c as *mut char as *mut u8, 1);
         if x != 1 {
-            return x;
+            return 2;
         }
-    }
 
-    let x: usize = unsafe {
         let fd: usize = open(str::as_ptr("/tmp/test.txt\0") as *mut u8, O_RDONLY, 0);
-        let mut buf: char = 69 as u8 as char;
+        if fd == usize_MAX {
+            return 1;
+        }
+        let mut buf: char = 10 as u8 as char;
         let x: usize = read(fd, &mut buf as *mut char as *mut u8, 1);
         if x != 1 {
-            return x;
+            return 3;
         }
         buf as usize
-    };
-
-    x
+    }
 }
 
 fn main() {
