f9b1e32b80d4350abeff60224768459a3f0b89e8
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-06-17 10:00:40 +0200

    test generics with enums

---
+++ tests/rust/generic_using_enums.rs
@@ -0,0 +1,26 @@
+enum Entry {
+    Entry(usize, usize),
+}
+
+fn allocate<T>(value: T) -> *mut T {
+    let p: *mut T = unsafe { malloc(size_of::<T>()) as *mut T };
+    unsafe { *p = value };
+    p
+}
+
+fn test() -> usize {
+    let p: *mut Entry = allocate::<Entry>(Entry::Entry(22, 20));
+    unsafe {
+        let Entry::Entry(a, b): &Entry = &*p;
+        *a + *b
+    }
+}
+
+fn main() {
+    unsafe { exit(test()) }
+}
+
+unsafe extern "C" {
+    fn exit(code: usize) -> !;
+    fn malloc(size: usize) -> *mut u8;
+}
