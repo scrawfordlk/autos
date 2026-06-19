a09b2eec1172ec461693c819cfe3460d274a8e5d
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-06-20 00:42:58 +0200

    include a simple generic function using an enum in test

--- tests/rust/generic_using_enums.rs
+++ tests/rust/generic_using_enums.rs
@@ -2,6 +2,10 @@ enum Entry {
     Entry(usize, usize),
 }
 
+fn id<T>(e: T) -> T {
+    e
+}
+
 fn allocate<T>(value: T) -> *mut T {
     let p: *mut T = unsafe { malloc(size_of::<T>()) as *mut T };
     unsafe { *p = value };
@@ -9,6 +13,7 @@ fn allocate<T>(value: T) -> *mut T {
 }
 
 fn test() -> usize {
+    id::<Entry>(Entry::Entry(0, 0));
     let p: *mut Entry = allocate::<Entry>(Entry::Entry(22, 20));
     unsafe {
         let Entry::Entry(a, b): &Entry = &*p;
